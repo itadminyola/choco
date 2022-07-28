d2aac0a3387d791a00c07ae5384bf5d53084af31
itadminyola <itadmin@yola.vn>
2022-07-28 17:27:13 +0700

    update commit

--- 7zipMdt/tools/chocolateyinstall.ps1
+++ 7zipMdt/tools/chocolateyinstall.ps1
@@ -10,28 +10,19 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $fileLocation = Join-Path $toolsDir '7z2200-x64.exe'
 
 
-# Internal packages (organizations) or software that has redistribution rights (community repo)
-# - Use `Install-ChocolateyInstallPackage` instead of `Install-ChocolateyPackage`
-#   and put the binaries directly into the tools folder (we call it embedding)
-#$fileLocation = Join-Path $toolsDir 'NAME_OF_EMBEDDED_INSTALLER_FILE'
-# If embedding binaries increase total nupkg size to over 1GB, use share location or download from urls
-#$fileLocation = '\\SHARE_LOCATION\to\INSTALLER_FILE'
-# Community Repo: Use official urls for non-redist binaries or redist where total package size is over 200MB
-# Internal/Organization: Download from internal location (internet sources are unreliable)
-# $url = '' # download url, HTTPS preferred
-# $url64 = '' # 64bit URL here (HTTPS preferred) or remove - if installer contains both (very rare), use $url
+
 
 $packageArgs = @{
-  packageName    = $env:7zipMdt
-  # unzipLocation  = $toolsDir
-  fileType       = 'EXE_MSI' #only one of these: exe, msi, msu
+  packageName    = $env:ChocolateyPackageName
+  unzipLocation  = $toolsDir
+  fileType       = 'EXE' #only one of these: exe, msi, msu
   # url            = $url
   # url64bit       = $url64
   file           = $fileLocation
-  softwareName   = '7zip' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
+  softwareName   = '7zip*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
   # MSI
-  silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
-  validExitCodes = @(0, 3010, 1641)
+  silentArgs     = "/S `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
+  validExitCodes = @(0)
   
 }
 
--- 7zipMdt/tools/chocolateyuninstall.ps1
+++ 7zipMdt/tools/chocolateyuninstall.ps1
@@ -12,15 +12,15 @@
 
 $ErrorActionPreference = 'Stop'; # stop on all errors
 $packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  softwareName  = '7zipMdt*'  #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
-  fileType      = 'EXE_MSI_OR_MSU' #only one of these: MSI or EXE (ignore MSU for now)
+  packageName    = $env:ChocolateyPackageName
+  softwareName   = '7zipMdt*'  #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
+  fileType       = 'EXE' #only one of these: MSI or EXE (ignore MSU for now)
   # MSI
-  silentArgs    = "/qn /norestart"
-  validExitCodes= @(0, 3010, 1605, 1614, 1641) # https://msdn.microsoft.com/en-us/library/aa376931(v=vs.85).aspx
+  # silentArgs    = "/qn /norestart"
+  validExitCodes = @(0) # https://msdn.microsoft.com/en-us/library/aa376931(v=vs.85).aspx
   # OTHERS
   # Uncomment matching EXE type (sorted by most to least common)
-  #silentArgs   = '/S'           # NSIS
+  silentArgs     = '/S'           # NSIS
   #silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' # Inno Setup
   #silentArgs   = '/s'           # InstallShield
   #silentArgs   = '/s /v"/qn"'   # InstallShield with MSI
@@ -30,7 +30,7 @@ $packageArgs = @{
   #silentArgs   = '-s -u'        # Ghost
   # Note that some installers, in addition to the silentArgs above, may also need assistance of AHK to achieve silence.
   #silentArgs   = ''             # none; make silent with input macro script like AutoHotKey (AHK)
-                                 #       https://community.chocolatey.org/packages/autohotkey.portable
+  #       https://community.chocolatey.org/packages/autohotkey.portable
   #validExitCodes= @(0) #please insert other valid exit codes here
 }
 
@@ -55,7 +55,8 @@ if ($key.Count -eq 1) {
       # Alternatively if you need to pass a path to an msi, determine that and
       # use it instead of the above in silentArgs, still very first
       $packageArgs['file'] = ''
-    } else {
+    }
+    else {
       # NOTES:
       # - You probably will need to sanitize $packageArgs['file'] as it comes from the registry and could be in a variety of fun but unusable formats
       # - Split args from exe in $packageArgs['file'] and pass those args through $packageArgs['silentArgs'] or ignore them
@@ -65,13 +66,15 @@ if ($key.Count -eq 1) {
 
     Uninstall-ChocolateyPackage @packageArgs
   }
-} elseif ($key.Count -eq 0) {
+}
+elseif ($key.Count -eq 0) {
   Write-Warning "$packageName has already been uninstalled by other means."
-} elseif ($key.Count -gt 1) {
+}
+elseif ($key.Count -gt 1) {
   Write-Warning "$($key.Count) matches found!"
   Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
   Write-Warning "Please alert package maintainer the following keys were matched:"
-  $key | % {Write-Warning "- $($_.DisplayName)"}
+  $key | % { Write-Warning "- $($_.DisplayName)" }
 }
 
 ## OTHER POWERSHELL FUNCTIONS
